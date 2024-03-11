ab6632b07ebff37b07bc3d2952ae127804e1b2ae
kittimas <kittimasdev@gmail.com>
2024-03-11 16:32:18 +0700

    ง

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,7 @@ import * as animationData_4 from '../public/anime/animation_5.json'
 import * as animationData_5 from '../public/anime/animation_5.json'
 import * as animationData_6 from '../public/anime/animation_6.json'
 import * as animationData_7 from '../public/anime/animation_7.json'
-
-
+import * as animationData_8 from '../public/anime/animetion_8.json'
 
 
 export default function Home() {
@@ -67,7 +66,7 @@ export default function Home() {
   const Ribbon = {
     loop: true,
     autoplay: true,
-    animationData: animationData_7,
+    animationData: animationData_8,
     rendererSettings: {
       preserveAspectRatio: 'xMidYMid slice'
     }
@@ -100,16 +99,16 @@ export default function Home() {
       <div className="card animate__animated animate__tada">
         <Flower >
           {
-            // HappyTime &&
-            // <Lottie
-            //   options={defaultOptions}
-            //   height={"100%"}
-            //   width={"100%"}
-            //   isStopped={false}
-            //   isPaused={false}
-            // />
+            HappyTime &&
+            <Lottie
+              options={defaultOptions}
+              height={"100%"}
+              width={"100%"}
+              isStopped={false}
+              isPaused={false}
+            />
           }
-                   {
+          {
             // HappyTime &&
             // <Lottie
             //   options={Ribbon}
@@ -144,24 +143,22 @@ export default function Home() {
         </div>
         <BallLayout>
           {
-            // BallShow &&
-            // <Lottie
-            //   options={Ball}
-            //   height={"100%"}
-            //   width={"100%"}
-            //   isStopped={false}
-            //   isPaused={false}
-            // />
+            BallShow &&
+            <Lottie
+              options={Ball}
+              height={"100%"}
+              width={"100%"}
+              isStopped={false}
+              isPaused={false}
+            />
           }
         </BallLayout>
-
-      </div>
       <Fireworks_Right>
         <div data-fireworks='right'>
           {
-            BallShow2 &&
+            HappyTime &&
             <Lottie
-              options={Fireworks}
+              options={Ribbon}
               height={"100%"}
               width={"100%"}
               isStopped={false}
@@ -170,12 +167,27 @@ export default function Home() {
           }
         </div>
       </Fireworks_Right>
-      {/* <Fireworks_Right>
+      </div>
+      <Fireworks_Right>
+        <div data-fireworks='right'>
+          {/* {
+            BallShow2 &&
+            <Lottie
+              options={Fireworks}
+              height={"100%"}
+              width={"100%"}
+              isStopped={false}
+              isPaused={false}
+            />
+          } */}
+        </div>
+      </Fireworks_Right>
+      <Fireworks_Right>
         <div data-fireworks='right'>
           {
             HappyTime &&
             <Lottie
-              options={FireworksRight}
+              options={Ribbon}
               height={"100%"}
               width={"100%"}
               isStopped={false}
@@ -183,7 +195,7 @@ export default function Home() {
             />
           }
         </div>
-      </Fireworks_Right> */}
+      </Fireworks_Right>
     </body>
   );
 }
@@ -230,12 +242,12 @@ const Fireworks_left = styled.div`
 
 const Fireworks_Right = styled.div`
     /* width: 100%; */
-    height: 100%;
+    /* height: 100%; */
     position: absolute;
-    top: 50%;
-    left: 50%;
+    /* top: 50%;
+    left: 50%; */
     pointer-events: none;
-    transform: translate(-50%, -50%);
+    /* transform: translate(-50%, -50%); */
     [data-fireworks='right']
     {
       width: 100%;
